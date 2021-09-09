18c076dda4b378e7be30b1b7535332c7ce720c49
Ayo Akindele <a.akindele01@gmail.com>
2021-09-09 09:52:34 -0600

    download script from GitHub and print source code

---
+++ README.md
@@ -0,0 +1,3 @@
+# CMPUT 404 Lab 1
+
+[Lab 1 - Virtualenv & cURL](https://uofa-cmput404.github.io/lab-1-virtualenv-curl.html)
--- lab1.py
+++ lab1.py
@@ -3,3 +3,6 @@ import requests
 print("requests version:", requests.__version__)
 print("response for GET 'http://www.google.com/':",
       requests.get("http://www.google.com/"))
+print("\nlab 1 python script source code:\n```\n" + 
+      requests.get("https://raw.githubusercontent.com" +
+                   "/aakindel/404_lab_1/master/lab1.py").text + "```")
